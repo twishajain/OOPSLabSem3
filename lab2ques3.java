ceba021af0b78513a4281a2870849850f18f3959
twishajain <33196561+twishajain@users.noreply.github.com>
2018-08-07 22:14:59 +0530

    Add files via upload

---
+++ lab2ques3.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author student
+ */
+
+import java.util.Scanner;
+public class lab2ques3 {
+    public static void main(String[] args){
+    Scanner s1=new Scanner(System.in);
+    
+    int s= s1.nextInt();
+    System.out.println(s<<1);
+    System.out.println(s>>1);
+    
+    
+    
+}}
