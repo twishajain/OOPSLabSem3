e37205e04b65e7aab7803c30d7840af1cf68e647
twishajain <33196561+twishajain@users.noreply.github.com>
2018-08-07 17:28:27 +0530

    Add files via upload

---
+++ tableofnum.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class tableofnum
+{public static void main(String args[])
+	{
+		Scanner s1= new Scanner(System.in);
+		int c;
+		c=s1.nextInt();
+		for (int i=1;i<11;i++)
+			System.out.println(c+"*"+i+"="+c*i);
+}}
+	
