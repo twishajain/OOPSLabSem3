4b3a7072fff55e050a4b642ea9fd6a48635ecce1
twishajain <33196561+twishajain@users.noreply.github.com>
2018-08-07 22:16:49 +0530

    Add files via upload

---
+++ lab2ques4.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author student
+ */
+public class lab2ques4 {
+    public static void main(String[] args)
+    {
+        int x=10;
+        double y=x;
+        System.out.println(y);
+        double a=10.5;
+        int b=(int)a;
+  
+        System.out.println(b);
+    }
+}
