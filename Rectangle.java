9c4c82e8caa6d41e91630edc74a0ddefebdae1a9
twishajain <33196561+twishajain@users.noreply.github.com>
2018-08-07 17:31:48 +0530

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,15 @@
+class Rectangle
+{
+	public static void main(String args[])
+	{
+		int m,n,i,j,a,b;
+		m=Integer.parseInt(args[0]);
+		n=Integer.parseInt(args[1]);
+		i=n*m;
+		a=n+m;
+		j=2*a;
+		System.out.println("Area="+i);
+		System.out.println("Perimeter="+j);
+
+	}
+}
