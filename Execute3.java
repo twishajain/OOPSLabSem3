f74aab0d295d637e7144d3f6edbf3082d6d7c6e1
twishajain <33196561+twishajain@users.noreply.github.com>
2018-08-08 21:14:33 +0530

    Create Execute3.java

---
+++ Execute3.java
@@ -0,0 +1,14 @@
+
+public class Execute3
+{
+    public static void main(String args[])
+    {
+        boolean x = true;
+       // int y = x;
+        //System.out.println("Part A: "+y);
+        int y = (int)x;
+        System.out.println("Part A: "+y);
+
+
+    }
+}
