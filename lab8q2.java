ae999d678620004138e5f48a89a619afa5d753fd
twishajain <33196561+twishajain@users.noreply.github.com>
2018-10-07 21:21:07 +0530

    Update lab8q2.java

--- lab8q2.java
+++ lab8q2.java
@@ -1,17 +1,19 @@
 abstract class Area{
-abstract void calc();     //abstract class
+abstract void calc();   //abstract class
 }
-class Circle extends Area{
+class Circle extends Area{int r;
+Circle()
+{r=1;}
 void calc()
 {
 
-System.out.println((3.14)*1*1);       //calc area of circle 
+System.out.println((3.14)*r*r);   //calc area of circle
 }
 }
-class Rectangle extends Area{
+class Rectangle extends Area{int l,b;Rectangle(){l=1;b=2;}
 void calc()
 {
-System.out.println(1*2);      //calc area of rectangle
+System.out.println(l*b);    //calc area of rectangle
 }
 }
 class lab8q2{
