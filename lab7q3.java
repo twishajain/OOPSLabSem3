1a4dd08e4eb29b3a405f2b44fde296c54b95f909
twishajain <33196561+twishajain@users.noreply.github.com>
2018-10-07 22:23:05 +0530

    Update lab7q3.java

--- lab7q3.java
+++ lab7q3.java
@@ -1,25 +1,24 @@
-  
-class lab7q3 
-{ 
-    public static void main (String[] args) 
-    { 
-        // creating an Integer Object 
-        // with value 10. 
-        Integer i = new Integer(10); 
-  
-        // unboxing the Object 
-        int i1 = i; 
-  
-        System.out.println("Value of i: " + i); 
-        System.out.println("Value of i1: " + i1); 
-  
-        //Autoboxing of char 
-        Character gfg = 'a'; 
-  
-        // Auto-unboxing of Character 
-        char ch = gfg; 
-        System.out.println("Value of ch: " + ch); 
-        System.out.println("Value of gfg: " + gfg); 
-  
-    } 
+class lab7q3
+{
+    public static void main (String args[])
+    {
+        // creating an Integer Object
+        // with value 10.
+        Integer i = 10;
+
+        // unboxing the Object
+        int i1 = i;
+
+        System.out.println("Value of i: " + i);
+        System.out.println("Value of i1: " + i1);
+
+        //Autoboxing of char
+        Character gfg = 'a';
+
+        // Auto-unboxing of Character
+        char ch = gfg;
+        System.out.println("Value of ch: " + ch);
+        System.out.println("Value of gfg: " + gfg);
+
+    }
 }
